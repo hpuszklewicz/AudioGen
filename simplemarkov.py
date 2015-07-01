a38d5d94d88ed99a46f364339bd8af17c844b1e2
Hubert <hubertpuszklewicz@gmail.com>
2015-07-01 15:41:34 -0400

    added graph for markov chains

--- simplemarkov.py
+++ simplemarkov.py
@@ -1,11 +1,63 @@
 class MarkovChain():
-    import random
-    def __init__(self, chain, curr = None):
-        self.chain = chain
-        self.currState = random.choice(chain) if curr is not None else curr
+    def __init__(self):
+        self.graph = Graph()
 
-    def nextState(given = None):
-        if given == None:
-            currIndex = self.chain.index(self.currState)
-            return self.chain[currIndex + 1]
+    def addState(self, state):
+        self.graph.addVertex(Vertex(state))
+    
+    def connect(self, state1, state2, weight):
+        if weight < 0 or weight > 1:
+            raise ValueError("Weights in a markov chain must be between 0 and 1.")
+        vertex1 = self.graph.vertices[state1]
+        vertex2 = self.graph.vertices[state2]
+        vertex1.connectTo(vertex2, weight)
+
+    def __str__(self):
+        return self.graph.__str__()
         
+
+class Vertex():
+    def __init__(self, name):
+        self.name = name
+        self.neighbors = {}
+
+    def connectTo(self, vertex, weight):
+        self.neighbors[vertex] = weight
+        vertex.neighbors[self] = weight
+
+    def __str__(self):
+        info = ""
+        info += self.name + " is connected to: "
+        for neighbor in self.neighbors:
+            info += "(" + neighbor.name + ", " 
+            info += str(self.neighbors[neighbor]) + ") "
+        return info
+
+class Graph():
+    def __init__(self):
+        self.vertices = {}
+    
+    def addVertex(self, vertex):
+        self.vertices[vertex.name] = vertex
+
+    def __str__(self):
+        info = ""
+        for vertex in self.vertices:
+            info += vertex + ": "
+            neighbors = self.vertices[vertex].neighbors
+            for neighbor in neighbors:
+                info += "(" + neighbor.name + ", " 
+                info += str(neighbors[neighbor]) + ") "
+            info += "\n"
+        return info
+
+if __name__=='__main__':
+    mc = MarkovChain()
+    mc.addState("A")
+    mc.addState("B")
+    mc.addState("C")
+    mc.addState("D")
+    mc.connect("A", "B", 0.7)
+    mc.connect("A", "C", 0.34)
+    mc.connect("C", "D", 0.289)
+    print mc
