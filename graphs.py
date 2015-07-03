df8a2096eaf2026cbf8155002ea466cb6c116e0f
Hubert <hubertpuszklewicz@gmail.com>
2015-07-03 16:24:17 -0400

    improved graph classes

--- graphs.py
+++ graphs.py
@@ -1,86 +1,44 @@
 import random
 
-class MarkovChain():
-    def __init__(self):
-        self.graph = Graph()
-        self.curr = None
-
-    def addState(self, state):
-        self.graph.addVertex(Vertex(state))
-    
-    def connect(self, state1, state2, weight):
-        if weight < 0 or weight > 1:
-            raise ValueError("Weights in a markov chain must be between 0 and 1.")
-        vertex1 = self.graph.vertices[state1]
-        vertex2 = self.graph.vertices[state2]
-        vertex1.connectTo(vertex2, weight)
-        if not self.checkNeighborhoodSums():
-            vertex1.disconnectFrom(vertex2)
-            raise ValueError("Sum of neighborhood edges must be between 0 and 1.")
-
-    def getNeighborhoodSum(self, vertex):
-        total = 0
-        graph = self.graph.vertices
-        neighbors = graph[vertex].neighbors
-        for neighbor in neighbors:
-            total += neighbors[neighbor]
-        return total
-
-    def checkNeighborhoodSums(self):
-        graph = self.graph.vertices
-        for vertex in graph:
-            total = 0
-            neighbors = graph[vertex].neighbors
-            for neighbor in neighbors:
-                total += neighbors[neighbor]
-            if total > 1:
-                return False
-        return True
-
-    def start(self, start = None):
-        choices = self.graph.vertices
-        if start is None:
-            self.curr = random.choice(list(choices.keys()))
-        else:
-            self.curr = start
-        print "Starting at: " + self.curr
-
-    def getNextState(self):
-        choices = self.graph.vertices
-        nextChoices = choices[self.curr]
-        for choice in nextChoices.neighbors:
-            print self.getNeighborhoodSum(choice.name)
-            
-    def __str__(self):
-        return self.graph.__str__()
-        
-
 class Vertex():
-    def __init__(self, name):
+    def __init__(self, name, directed = False):
         self.name = name
         self.neighbors = {}
+        self.directed = directed
 
     def connectTo(self, vertex, weight):
         self.neighbors[vertex] = weight
-        vertex.neighbors[self] = weight
 
     def disconnectFrom(self, vertex):
         self.neighbors.pop(vertex, None)
 
     def __str__(self):
         info = ""
-        info += self.name + " is connected to: "
-        for neighbor in self.neighbors:
-            info += "(" + neighbor.name + ", " 
-            info += str(self.neighbors[neighbor]) + ") "
-        return info
+        if len(self.neighbors) == 0:
+            return self.name + " is isolated."
+            info += self.name + " is connected to: "
+            for neighbor in self.neighbors:
+                info += "(" + neighbor.name + ", " 
+                info += str(self.neighbors[neighbor]) + ") "
+                return info
 
 class Graph():
     def __init__(self):
         self.vertices = {}
-    
-    def addVertex(self, vertex):
-        self.vertices[vertex.name] = vertex
+        self.directed = False
+        
+    def addVertex(self, name):
+        self.vertices[name] = Vertex(name)
+
+    def connect(self, name1, name2, weight):
+        vertex1, vertex2 = self.vertices[name1], self.vertices[name2]
+        vertex1.connectTo(vertex2, weight)
+        vertex2.connectTo(vertex1, weight)
+
+    def disconnect(self, name1, name2):
+        vertex1, vertex2 = self.vertices[name1], self.vertices[name2]
+        vertex1.disconnectFrom(vertex2)
+        vertex2.disconnectFrom(vertex1)
 
     def __str__(self):
         info = ""
@@ -90,36 +48,55 @@ class Graph():
             for neighbor in neighbors:
                 info += "(" + neighbor.name + ", " 
                 info += str(neighbors[neighbor]) + ") "
-            info += "\n"
-        return info
+                info += "\n"
+                return info
 
-if __name__=='__main__':
-    g = Graph()
-    v1 = Vertex("A")
-    v2 = Vertex("B")
-    v3 = Vertex("C")
-    v4 = Vertex("D")
-    v1.connectTo(v2, 3)
-    print v1
-    v1.disconnectFrom(v2)
-    print v1
+class DirectedGraph(Graph):
+    def __init__(self):
+        Graph.__init__(self)
 
+    def connect(self, name1, name2, weight):
+        vertex1, vertex2 = self.vertices[name1], self.vertices[name2]
+        vertex1.connectTo(vertex2, weight)
+
+    def disconnect(self, name1, name2):
+        vertex1, vertex2 = self.vertices[name1], self.vertices[name2]
+        vertex1.disconnectFrom(vertex2)
+
+class MarkovChain(DirectedGraph):
+    def __init__(self):
+        DirectedGraph.__init__(self)
+
+    def addState(self, name):
+        self.vertices[name] = Vertex(name)
+
+    def connect(self, name1, name2, weight):
+        if weight < 0 or weight > 1:
+            raise ValueError("Weights in the chain must be between 0 and 1, inclusive")
+            vertex1, vertex2 = self.vertices[name1], self.vertices[name2]
+            vertex1.connectTo(vertex2, weight)
+            if not self.checkNeighborhoodSums():
+                raise ValueError("Sum of neighborhood edges must be between 0 and 1, inclusive")
+                
+    def checkNeighborhoodSums(self):
+        for vertex in self.vertices:
+            total = 0
+            neighbors = self.vertices[vertex].neighbors
+            for neighbor in neighbors:
+                total += neighbors[neighbor]
+                if total > 1:
+                    return False
+                    return True
+                    
+
+if __name__ == "__main__":
+    
     mc = MarkovChain()
     mc.addState("A")
+    print mc
     mc.addState("B")
     mc.addState("C")
-    mc.addState("D")
-    mc.connect("A", "B", 0.34)
+    mc.connect("A", "B", 0.2)
+    mc.connect("B", "A", 0.1)
+    mc.connect("A", "C", 0.78)
     print mc
-    mc.connect("A", "C", 0.65)
-    print mc
-    mc.connect("B", "D", 0.22394)
-    print mc
-    try:
-        mc.connect("A", "D", 0.04)
-    except ValueError:
-        print "Perfect"
-    print mc
-
-    mc.start()
-    mc.getNextState()
