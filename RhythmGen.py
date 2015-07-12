5b8af6ae281e507bcf411dae1fc5952a9af88243
Pavel Kondratyev <paulkondratyev@hotmail.com>
2015-07-12 10:41:19 +0200

    Added a function that equally spaces n hits in m beats in a list, as well as a function that makes a string out of that list

---
+++ RhythmGen.py
@@ -0,0 +1,47 @@
+# equally spaces the amount of desired pulses(or hits)
+# into the amount of desired steps(or beats)
+def bjorklund(steps, pulses):
+    if pulses > steps:
+        raise ValueError("Pulses cannot be greater than steps")
+    divisor = steps - pulses
+    remainder = [pulses]
+    level = 0
+    count = []
+
+    while True:
+        count.append(divisor/remainder[level])
+        remainder.append(divisor % remainder[level])
+        divisor = remainder[level]
+        level += 1
+        if remainder[level] <= 1:
+            break
+    count.append(divisor)
+
+    rhythm = []
+    def build(level):
+        if level == -1:
+            rhythm.append("0")
+        elif level == -2:
+            rhythm.append("1")
+        else:
+            for i in xrange(0, count[level]):
+                build(level - 1)
+            if remainder[level] != 0:
+                build(level - 2)
+    build(level)
+    i = rhythm.index("1")
+    rhythm = rhythm[i:] + rhythm[0:i]
+    return rhythm
+
+def toString(list):
+    rhythmString = ""
+    for i in xrange(0, len(list)):
+        rhythmString += list[i]
+    return rhythmString
+
+
+
+if __name__ == '__main__':
+    print bjorklund(16, 7)
+    print toString(bjorklund(16, 7))
+
