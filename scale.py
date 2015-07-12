0cb1b9383f2d5aecc6119204418cf72f3f4f8efa
Pavel Kondratyev <paulkondratyev@hotmail.com>
2015-07-12 21:25:22 +0200

    Added functions to create an array of intervals and consequently create a scale based on a given tonic and scale

---
+++ scale.py
@@ -0,0 +1,46 @@
+# returns the intervals in terms of half tones
+def getIntervals(scaleName):
+    if scaleName.lower() == "major":
+        return [2, 2, 1, 2, 2, 2, 1]
+    elif scaleName.lower() == "minor":
+        return [2, 1, 2, 2, 1, 2, 2]
+    elif scaleName.lower() == "harmonic minor":
+        return [2, 1, 2, 2, 1, 3, 1]
+    elif scaleName.lower() == "minor pentatonic":
+        return [3, 2, 2, 3, 2]
+    elif scaleName.lower() == "major pentatonic":
+        return [2, 2, 3, 2, 3]
+    elif scaleName.lower() == "blues scale":
+        return [3, 2, 1, 1, 3, 2]
+    elif scaleName.lower() == "ionian":
+        return [2, 2, 1, 2, 2, 2, 1]
+    elif scaleName.lower() == "dorian":
+        return [2, 1, 2, 2, 2, 1, 2]
+    elif scaleName.lower() == "phrygian":
+        return [1, 2, 2, 2, 1, 2, 2]
+    elif scaleName.lower() == "lydian":
+        return [2, 2, 2, 1, 2, 2, 1]
+    elif scaleName.lower() == "mixolydian":
+        return [2, 2, 1, 2, 2, 1, 2]
+    elif scaleName.lower() == "aeolian":
+        return [2, 1, 2, 2, 1, 2, 2]
+    elif scaleName.lower() == "locrian":
+        return [1, 2, 2, 1, 2, 2, 2]
+
+def getScale(tonic, intervals):
+    notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    for i in xrange(0, len(notes)/2 - 1):
+        if notes[i].lower() == tonic.lower():
+            rootIndex = i
+            break
+    scaleOutput = [notes[rootIndex]]
+
+    for i in xrange(0, len(intervals)):
+        rootIndex += intervals[i]
+        scaleOutput.append(notes[rootIndex])
+    return scaleOutput
+
+if __name__ == "__main__":
+    intervals = getIntervals("harmonic minor")
+    print intervals
+    print getScale("A", intervals)
